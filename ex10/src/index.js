38df32e0dc60e5b717fbf6fe0c9a6b7153f567df
Meša Selimović <mesa_sel@hotmail.com>
2021-03-11 14:32:32 +0100

    Declare String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+    // Only change code below this line
+    var myFirstName = "Meša";
+    var myLastName = "Selimović";
+    var myHomeTown = "Sarajevo";
+
+    // Only change code above this line
+
+return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+};
+
+}
+
+console.log(main());            // Change this line   
+
+module.exports = main;
