beebb773d32f1b73400e169d1300f6649efb7f2d
Meša Selimović <mesa_sel@hotmail.com>
2021-03-11 11:22:51 +0100

    Finding a Remainder

---
+++ ex07/src/index.js
@@ -0,0 +1,20 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var numOne;
+    var numTwo;
+    var remainder;
+    remainder = numOne % numTwo;
+    parseInt(numOne);
+    parseInt(numTwo);
+    // Only change code above this line
+
+return remainder;
+
+
+}
+
+console.log(main(5, 2));            // Change this line   
+console.log(main(15, 4));            // Change this line   
+console.log(main(102, 25));            // Change this line   
+
+module.exports = main;
